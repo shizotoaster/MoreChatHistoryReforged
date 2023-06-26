5706fbab6c01a197641eb6230eb5950a5a192e88
Mixer <51480483+shizotoaster@users.noreply.github.com>
2023-06-26 16:00:17 +0700

    Add files via upload

---
+++ src/main/java/net/shizotoaster/morechathistory/MoreChatHistoryReforged.java
@@ -0,0 +1,11 @@
+package net.shizotoaster.morechathistory;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MoreChatHistoryReforged.MODID)
+public class MoreChatHistoryReforged {
+    public static final String MODID = "morechathistory";
+    public MoreChatHistoryReforged() {
+
+    }
+}
